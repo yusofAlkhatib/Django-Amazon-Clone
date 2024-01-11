1e1af852c56b7369a9960c0a39a0c21a2863be68
yusofalkhatib <yusof94@hotmail.com>
2024-01-11 13:18:49 +0100

    design producte models part 1

---
+++ analysis.md
@@ -0,0 +1,17 @@
+
+Products:
+    -name
+    -image
+    -images*
+    -price
+    -subtitle
+    -description
+    -brand*
+    -sku
+    -reviews*
+    -SPECIFICATIONS
+    -video link
+    -tags
+    -quantity
+    -flag
+    
--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as
+
+FLAG_CHOICES = (
+    ('New','New'),
+    ('Sale','Sale'),
+    ('Feature','Feature'),
+)
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(_('Product Name'),max_length=120)
+    image = models.ImageField(_('Image'),upload_to='products')
+    price = models.FloatField(_('Price'))
+    subtitle = models.TextField(_('Subtitle'),max_length=500)
+    description = models.TextField(_('Description'),max_length=50000)
+    sku = models.IntegerField(_('Sku'))
+    video = models.URLField(_('Video'),null=True,blank=True)
+    quantity = models.IntegerField(_('Quantity'))
+    flag = models.CharField(_('Flag'),max_length=10,choices=FLAG_CHOICES)
+
+class ProductImages(models.Model):
+    pass
+
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='brands')
+
+
+
+class Review(models.Model):
+    user = ''
+    Product = ''
+    review = models.TextField(max_length=300)
+    rate = models.IntegerField()
+    Create_at = models.DateTimeField(timezone.now)
+
