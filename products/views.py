8b14144aa53371097d0459da518e5471c0b238bf
yusofalkhatib <yusof94@hotmail.com>
2024-01-13 12:17:33 +0100

    add products , brands views with urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DeleteView
+
+from .models import Product , Brand , Review
 
 # Create your views here.
+class ProductList(ListView):
+    model = Product
+
+
+
+class ProductDetail(DeleteView):
+    model = Product
+
+
+
+class BrandList(ListView):
+    model = Brand
+
+
+class BrandDetail(DeleteView):
+    model = Brand
+
