331f414080751eb55f6840cc5f21065a34ecfdc6
yusofalkhatib <yusof94@hotmail.com>
2024-02-12 20:07:15 +0100

    show productImges , reviews in Product API

--- products/serializers.py
+++ products/serializers.py
@@ -7,9 +7,22 @@ class BrandSerialzer(serializers.ModelSerializer):
         model = Brand
         fields = '__all__'
 
+class ProductImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    product_image = ProductImagesSerializer(many=True)
+    review_product = ReviewSerializer(many=True)
+    
     class Meta:
         model = Product
         fields ='__all__'
