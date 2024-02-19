0f4e980573e061ea9327868e0c4a3b81468fd50a
yusofalkhatib <yusof94@hotmail.com>
2024-02-19 21:31:00 +0100

    show data to home page

---
+++ settings/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from products.models import Product , Brand , Review
+
+# Create your views here.
+def home(request):
+    brands = Brand.objects.all()[:10]
+    sale = Product.objects.filter(flag='Nale')[:10]
+    new = Product.objects.filter(flag='New')[:10]
+    feature = Product.objects.filter(flag='Feature')[:6]
+    review = Review.objects.filter(rate=5)[:3]
+
+
+    return render(request,'settings/home.html',{
+        'brands':brands,
+        'sale': sale ,
+        'feature': feature,
+        'new': new,
+        'review':review
+    })
