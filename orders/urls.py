8e9cb12142101cf01ae7fb5ecd17495601de4c7c
yusofalkhatib <yusof94@hotmail.com>
2024-03-12 20:12:04 +0100

    add orders & checkout , process_payment , success , failed views structure

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import add_to_cart
+from .views import add_to_cart,order_list,checkout,process_payment,payment_success,payment_failed
 
 
 
 urlpatterns = [
+    
+     path('' , order_list),
+     path('checkout/' , checkout),
+     path('checkout/process_payment' , process_payment),
+     path('checkout/payment_success' , payment_success),
+     path('checkout/payment_failed' , payment_failed),
+
      path('add-to-cart' , add_to_cart),
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,40 @@ from django.shortcuts import render , redirect
 from .models import Cart , CartDetail
 from products.models import Product
 
+
+def order_list(request):
+    
+    return render(request,'orders/orders.html',{})
+
+
+
+def checkout(request):
+    # review order ,apply coupon
+
+    return render(request,'orders/checkout.html',{})
+
+
+def process_payment(request):
+    #process payment $
+    pass
+
+
+def payment_success(request):
+    # if payment was success
+
+    code = ''
+
+    return render(request,'orders/success.html',{'code':code})
+
+
+def payment_failed(request):
+    # if payment was filed
+    
+    return render(request,'orders/failed.html',{})
+
+
+
+
 def add_to_cart(request):
     product_id = request.POST['product_id']
     quantity = int(request.POST['quantity'])
