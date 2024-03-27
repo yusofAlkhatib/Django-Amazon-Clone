b41b751b34ed5f2eccec175ed540457ab5308c86
yusofalkhatib <yusof94@hotmail.com>
2024-03-27 17:27:16 +0100

    add orders instance methods for total , sub_total , discount

--- orders/models.py
+++ orders/models.py
@@ -7,6 +7,8 @@ from products.models import Product
 from accounts.models import Address
 from utils.generate_code import generate_code
 
+from settings.models import DeliveryFee
+
 
 ORDER_STATUS = (
     ('Recieved','Recieved'),
@@ -28,6 +30,31 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.user)
+    
+    def order_sub_total(self):
+        if self.coupon:
+            return self.order_total_discount
+        
+        total = 0
+        for item in self.order_detail.all():
+            total += item.total
+        return total
+
+    def order_total(self):
+        fee = DeliveryFee.objects.last().fee
+        return self.order_sub_total() + fee
+
+    def order_discount(self):
+        if self.coupon:
+            after_discount = self.order_sub_total()
+
+            before_discount = 0
+            for item in self.order_detail.all():
+                before_discount += item.total
+            return before_discount - after_discount
+        
+        else:
+            return 0
 
 
 class OrderDetail(models.Model):
