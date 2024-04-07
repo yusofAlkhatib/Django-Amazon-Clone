da8cdd6e1ef641237987c4cb035dfd826abecef3
yusofalkhatib <yusof94@hotmail.com>
2024-04-07 14:59:12 +0200

    add apply coupon api

--- orders/api.py
+++ orders/api.py
@@ -7,8 +7,7 @@ from .serializers import CartDetailsSerializer , CartSerializer , OrderSerialize
 
 from settings.models import DeliveryFee
 from products.models import Product
-
-
+import datetime
 
 class OrderListAPI(generics.ListAPIView):
     queryset = Order.objects.all()
@@ -39,8 +38,32 @@ class CreateOrderAPI(generics.GenericAPIView):
 
 
 class ApplyCouponAPI(generics.GenericAPIView):
-    pass
+    
+    def post(self,request,*args, **kwargs):
+        user = User.objects.get(self.kwargs['username'])  #get user using username : url
+        coupon = Coupon.objects.get(code = request.data['coupon_code']) #get coupon using coupon code comming from request body (mobile)
+        cart = Cart.objects.get(user=user , status='InProgress')
 
+        delivery_fee = DeliveryFee.objects.last().fee
+        sub_total = cart.cart_total()
+    
+
+
+        if coupon and coupon.quantity > 0:
+            today = datetime.datetime.today().date()
+            if today >= coupon.start_date and today <= coupon.end_date:
+                coupon_value = sub_total /100*coupon.discount
+                sub_total = sub_total - coupon_value
+
+                cart.coupon = coupon
+                cart.order_total_discount = sub_total
+                cart.save()
+                return Response({'message':'coupon was applied successfully'})
+            else:
+                return Response({'message':'coupon code date is not valid or expired'})
+            
+        else:
+            return Response({'message':'coupon code not found or ended.. '})
 
 
 class CartCreateUpdateDeleteAPI(generics.GenericAPIView):
