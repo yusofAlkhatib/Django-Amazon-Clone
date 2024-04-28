18780cf45809b91428c4121ae5f8a4f6be494495
yusofalkhatib <yusof94@hotmail.com>
2024-04-28 20:30:26 +0200

    add celery with redis Edit

--- project/settings.py
+++ project/settings.py
@@ -161,5 +161,7 @@ CACHES = {
 }
 
 
-CELERY_BROKER_URL="redis://localhost:6379"
-CELERY_RESULT_BACKEND ="redis://localhost:6379"
+# CELERY_BROKER_URL="redis://localhost:6379"
+# CELERY_RESULT_BACKEND ="redis://localhost:6379"
+CELERY_BROKER_URL="redis://redis:6379/0"
+CELERY_RESULT_BACKEND ="redis://redis:6379/0"
