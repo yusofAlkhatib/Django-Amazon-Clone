d67ff0d53d0ec88a1c8cdb96653450b724e3508d
yusofalkhatib <yusof94@hotmail.com>
2024-05-06 21:06:49 +0200

    design user models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Address
+from .models import Address , Profile , Phones
 
 
-
+admin.site.register(Profile)
+admin.site.register(Phones)
 admin.site.register(Address)
--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,24 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/')
+
+
+PHONE_TYPE = (
+    ('Primary' , 'Primary') ,
+    ('Secondry' , 'Secondry') ,
+)
+
+
+class Phones(models.Model):
+    user = models.ForeignKey(User,related_name='user_phone',on_delete=models.CASCADE)
+    type = models.CharField(max_length=20,choices=PHONE_TYPE)
+    number = models.CharField(max_length=25)
+
+    def __str__(self):
+        return str(self.user)
 
 
 ADDRESS_TYPE = (
